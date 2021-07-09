b6822edce98e15ee7f623b0473a888561bc7f304
littlepm96 <poscentemarco@hotmail.it>
2021-07-10 00:58:00 +0200

    k

--- src/main/java/com/example/ProgettoPennaWeb/persistenza/DatabaseManager.java
+++ src/main/java/com/example/ProgettoPennaWeb/persistenza/DatabaseManager.java
@@ -1,7 +1,5 @@
 package com.example.ProgettoPennaWeb.persistenza;
 
-import com.sun.org.apache.xml.internal.security.Init;
-
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
